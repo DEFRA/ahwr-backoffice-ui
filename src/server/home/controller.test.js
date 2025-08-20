e7f2f5dba0f0245f0b9f3ec50d2f2cc85672cde4
Ben Hope <bendef24@gmail.com>
2025-08-20 10:11:03 +0100

    feat(AHWR-1095): added claim details page, with faked data

--- src/server/home/controller.test.js
+++ src/server/home/controller.test.js
@@ -19,9 +19,7 @@ describe('#homeController', () => {
       url: '/'
     })
 
-    expect(result).toEqual(
-      expect.stringContaining('Claim number:')
-    )
+    expect(result).toEqual(expect.stringContaining('Claim number:'))
     expect(statusCode).toBe(statusCodes.ok)
   })
 })
