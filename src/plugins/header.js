61cb68821b4ad3e102d8113e11ecf8ec55b04e2a
Ben Hope <bendef24@gmail.com>
2025-09-22 16:57:11 +0100

    feat(AHWR-1185): fix authorisation, TEMP change to header plugin #patch

--- src/plugins/header.js
+++ src/plugins/header.js
@@ -1,16 +1,16 @@
-import { config } from '../config/config.js'
+// import { config } from '../config/config.js'
 
-const serviceUri = config.get('serviceUri')
+// const serviceUri = config.get('serviceUri')
 
-const getSecurityPolicy = () =>
-  "default-src 'self';" +
-  "object-src 'none';" +
-  "script-src 'self' www.google-analytics.com *.googletagmanager.com ajax.googleapis.com *.googletagmanager.com/gtm.js 'unsafe-inline' 'unsafe-eval' 'unsafe-hashes';" +
-  "form-action 'self';" +
-  "base-uri 'self';" +
-  "connect-src 'self' *.google-analytics.com *.analytics.google.com *.googletagmanager.com" +
-  "style-src 'self' 'unsafe-inline' tagmanager.google.com *.googleapis.com;" +
-  "img-src 'self' *.google-analytics.com *.googletagmanager.com;"
+// const getSecurityPolicy = () =>
+//   "default-src 'self';" +
+//   "object-src 'none';" +
+//   "script-src 'self' www.google-analytics.com *.googletagmanager.com ajax.googleapis.com *.googletagmanager.com/gtm.js 'unsafe-inline' 'unsafe-eval' 'unsafe-hashes';" +
+//   "form-action 'self';" +
+//   "base-uri 'self';" +
+//   "connect-src 'self' *.google-analytics.com *.analytics.google.com *.googletagmanager.com" +
+//   "style-src 'self' 'unsafe-inline' tagmanager.google.com *.googleapis.com;" +
+//   "img-src 'self' *.google-analytics.com *.googletagmanager.com;"
 
 export const headerPlugin = {
   plugin: {
@@ -40,8 +40,6 @@ export const headerPlugin = {
     }
   },
   options: {
-    keys: [
-      { key: 'Test', value: 'basic' }
-    ]
+    keys: [{ key: 'Test', value: 'basic' }]
   }
 }
