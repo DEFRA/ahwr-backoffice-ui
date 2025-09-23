c83375bc21c30dd7ca6f999c06d8648f3c24f280
Ben Hope <bendef24@gmail.com>
2025-09-23 17:58:38 +0100

    feat(AHWR-1185): changed header plugin #patch

--- src/plugins/header.js
+++ src/plugins/header.js
@@ -42,8 +42,8 @@ export const headerPlugin = {
   // TODO 1185 one of the commented out items is causing 502 error
   options: {
     keys: [
-      { key: 'X-Frame-Options', value: 'deny' },
-      { key: 'X-Content-Type-Options', value: 'nosniff' },
+      // { key: 'X-Frame-Options', value: 'deny' },
+      // { key: 'X-Content-Type-Options', value: 'nosniff' },
       // { key: 'Access-Control-Allow-Origin', value: serviceUri },
       // { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
       // { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
