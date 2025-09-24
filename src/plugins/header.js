f372c47796c404bb32671e16a6c974a3e975e46a
Ben Hope <bendef24@gmail.com>
2025-09-24 07:55:54 +0100

    feat(AHWR-1185): changed header plugin #patch

--- src/plugins/header.js
+++ src/plugins/header.js
@@ -50,8 +50,8 @@ export const headerPlugin = {
       // { key: 'X-Robots-Tag', value: 'noindex, nofollow' },
       // { key: 'X-XSS-Protection', value: '1; mode=block' },
       // { key: 'Strict-Transport-Security', value: 'max-age=31536000;' },
-      { key: 'Cache-Control', value: 'no-cache' },
-      { key: 'Referrer-Policy', value: 'no-referrer' }
+      { key: 'Cache-Control', value: 'no-cache' }
+      // { key: 'Referrer-Policy', value: 'no-referrer' }
       // {
       //   key: 'Content-Security-Policy',
       //   value: getSecurityPolicy()
