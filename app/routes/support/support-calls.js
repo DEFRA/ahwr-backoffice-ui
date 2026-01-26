5295da61175d448cb3910617b846651cd48c00f0
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-01-26 17:35:29 +0000

    Dealing with application reference not found

--- app/routes/support/support-calls.js
+++ app/routes/support/support-calls.js
@@ -1,6 +1,21 @@
-export const getApplicationDocument = async (applicationReference) => {
-  // const { payload } = await wreck.get(`https://somehwere/${applicationReference}`, { json: true });
+import wreck from "@hapi/wreck";
+import { config } from "../../config/index.js";
 
-  console.log("Retrieving application document");
-  return { document: { some: "value", another: "entry" } };
+const { applicationApiUri } = config;
+export const getApplicationDocument = async (applicationReference) => {
+  try {
+    const { payload } = await wreck.get(
+      `${applicationApiUri}/support/applications/${applicationReference}`,
+      {
+        json: true,
+      },
+    );
+    return payload;
+  } catch (error) {
+    if (error.data.res.statusCode === 404) {
+      return "No application found";
+    }
+
+    throw error;
+  }
 };
