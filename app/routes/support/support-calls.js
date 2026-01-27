8f0c31b268a0cb35a680fcff8153107c03784c0a
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-01-27 18:24:06 +0000

    Removed duplication of calls

--- app/routes/support/support-calls.js
+++ app/routes/support/support-calls.js
@@ -4,14 +4,12 @@ import { StatusCodes } from "http-status-codes";
 
 const { applicationApiUri, paymentProxyApiUri, messageGeneratorApiUri, documentGeneratorApiUri } =
   config;
-export const getApplicationDocument = async (applicationReference) => {
+
+const makeCall = async (url, parameter, notFoundMessage) => {
   try {
-    const { payload } = await wreck.get(
-      `${applicationApiUri}/support/applications/${applicationReference}`,
-      {
-        json: true,
-      },
-    );
+    const { payload } = await wreck.get(`${url}`, {
+      json: true,
+    });
     return payload;
   } catch (error) {
     if (error.data.res.statusCode === StatusCodes.NOT_FOUND) {
@@ -22,104 +20,45 @@ export const getApplicationDocument = async (applicationReference) => {
   }
 };
 
-export const getClaimDocument = async (claimReference) => {
-  try {
-    const { payload } = await wreck.get(`${applicationApiUri}/support/claims/${claimReference}`, {
-      json: true,
-    });
-    return payload;
-  } catch (error) {
-    if (error.data.res.statusCode === StatusCodes.NOT_FOUND) {
-      return "No claim found";
-    }
+export const getApplicationDocument = async (applicationReference) => {
+  return makeCall(
+    `${applicationApiUri}/support/applications/${applicationReference}`,
+    "No application found",
+  );
+};
 
-    throw error;
-  }
+export const getClaimDocument = async (claimReference) => {
+  return makeCall(`${applicationApiUri}/support/claims/${claimReference}`, "No claim found");
 };
 
 export const getHerdDocument = async (herdId) => {
-  try {
-    const { payload } = await wreck.get(`${applicationApiUri}/support/herds/${herdId}`, {
-      json: true,
-    });
-    return payload;
-  } catch (error) {
-    if (error.data.res.statusCode === StatusCodes.NOT_FOUND) {
-      return "No herd found";
-    }
-
-    throw error;
-  }
+  return makeCall(`${applicationApiUri}/support/herds/${herdId}`, "No claim found");
 };
 
 export const getPaymentDocument = async (paymentReference) => {
-  try {
-    const { payload } = await wreck.get(
-      `${paymentProxyApiUri}/support/payments/${paymentReference}/request-status`,
-      {
-        json: true,
-      },
-    );
-    return payload;
-  } catch (error) {
-    if (error.data.res.statusCode === StatusCodes.NOT_FOUND) {
-      return "No payment found";
-    }
-
-    throw error;
-  }
+  return makeCall(
+    `${paymentProxyApiUri}/support/payments/${paymentReference}/request-status`,
+    "No payment found",
+  );
 };
 
 export const getAgreementMessagesDocument = async (agreementReference) => {
-  try {
-    const { payload } = await wreck.get(
-      `${messageGeneratorApiUri}/support/message-generation?agreementReference=${agreementReference}`,
-      {
-        json: true,
-      },
-    );
-    return payload;
-  } catch (error) {
-    if (error.data.res.statusCode === StatusCodes.NOT_FOUND) {
-      return "No agreement messages found";
-    }
-
-    throw error;
-  }
+  return makeCall(
+    `${messageGeneratorApiUri}/support/message-generation?agreementReference=${agreementReference}`,
+    "No agreement messages found",
+  );
 };
 
 export const getClaimMessagesDocument = async (claimReference) => {
-  try {
-    const { payload } = await wreck.get(
-      `${messageGeneratorApiUri}/support/message-generation?claimReference=${claimReference}`,
-      {
-        json: true,
-      },
-    );
-    return payload;
-  } catch (error) {
-    if (error.data.res.statusCode === StatusCodes.NOT_FOUND) {
-      return "No claim message found";
-    }
-
-    throw error;
-  }
+  return makeCall(
+    `${messageGeneratorApiUri}/support/message-generation?claimReference=${claimReference}`,
+    "No claim message found",
+  );
 };
 
 export const getAgreementLogsDocument = async (agreementReference) => {
-  try {
-    const { payload } = await wreck.get(
-      `${documentGeneratorApiUri}/support/document-logs?agreementReference=${agreementReference}`,
-      {
-        json: true,
-      },
-    );
-    return payload;
-  } catch (error) {
-    if (error.data.res.statusCode === StatusCodes.NOT_FOUND) {
-      return "No Logs found";
-    }
-
-    throw error;
-  }
+  return makeCall(
+    `${documentGeneratorApiUri}/support/document-logs?agreementReference=${agreementReference}`,
+    "No Logs found",
+  );
 };
