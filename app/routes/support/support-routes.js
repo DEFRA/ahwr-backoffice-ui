209eb2b01eeadb0fe6a591d88aaff277a7a0afab
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-01-28 12:28:37 +0000

    "Two or more use a for" simplification of routes

--- app/routes/support/support-routes.js
+++ app/routes/support/support-routes.js
@@ -28,6 +28,21 @@ const getSupportRoute = {
   },
 };
 
+const actions = [
+  searchApplication,
+  searchClaim,
+  searchHerd,
+  searchPayment,
+  searchAgreementMessages,
+  searchClaimMessages,
+  searchAgreementLogs,
+];
+
+const actionHandlers = actions.reduce((acc, { action, handler }) => {
+  acc[action] = handler;
+  return acc;
+}, {});
+
 const postSupportRoute = {
   method: "POST",
   path: "/support",
@@ -37,36 +52,10 @@ const postSupportRoute = {
     },
     validate: {
       payload: Joi.alternatives().conditional(".action", {
-        switch: [
-          {
-            is: searchApplication.action,
-            then: searchApplication.validation,
-          },
-          {
-            is: searchClaim.action,
-            then: searchClaim.validation,
-          },
-          {
-            is: searchHerd.action,
-            then: searchHerd.validation,
-          },
-          {
-            is: searchPayment.action,
-            then: searchPayment.validation,
-          },
-          {
-            is: searchAgreementMessages.action,
-            then: searchAgreementMessages.validation,
-          },
-          {
-            is: searchClaimMessages.action,
-            then: searchClaimMessages.validation,
-          },
-          {
-            is: searchAgreementLogs.action,
-            then: searchAgreementLogs.validation,
-          },
-        ],
+        switch: actions.map((actionHandler) => ({
+          is: actionHandler.action,
+          then: actionHandler.validation,
+        })),
       }),
       failAction: async (request, h, error) => {
         const errors = error.details
@@ -76,32 +65,12 @@ const postSupportRoute = {
               return { ...receivedError, message: `Action ${action} is not supported.` };
             }
 
-            if (receivedError.message.includes(searchApplication.errorIdentifier)) {
-              return searchApplication.errorHandler(receivedError);
-            }
+            const match = actions.find((item) =>
+              receivedError.message.includes(item.errorIdentifier),
+            );
 
-            if (receivedError.message.includes(searchClaim.errorIdentifier)) {
-              return searchClaim.errorHandler(receivedError);
-            }
-
-            if (receivedError.message.includes(searchHerd.errorIdentifier)) {
-              return searchHerd.errorHandler(receivedError);
-            }
-
-            if (receivedError.message.includes(searchPayment.errorIdentifier)) {
-              return searchPayment.errorHandler(receivedError);
-            }
-
-            if (receivedError.message.includes(searchAgreementMessages.errorIdentifier)) {
-              return searchAgreementMessages.errorHandler(receivedError);
-            }
-
-            if (receivedError.message.includes(searchClaimMessages.errorIdentifier)) {
-              return searchClaimMessages.errorHandler(receivedError);
-            }
-
-            if (receivedError.message.includes(searchAgreementLogs.errorIdentifier)) {
-              return searchAgreementLogs.errorHandler(receivedError);
+            if (match) {
+              return match.errorHandler(receivedError);
             }
 
             return null;
@@ -118,27 +87,7 @@ const postSupportRoute = {
     },
     handler: async (request, h) => {
       const { action } = request.payload;
-      if (action === searchApplication.action) {
-        return searchApplication.handler(request, h);
-      }
-      if (action === searchClaim.action) {
-        return searchClaim.handler(request, h);
-      }
-      if (action === searchHerd.action) {
-        return searchHerd.handler(request, h);
-      }
-      if (action === searchPayment.action) {
-        return searchPayment.handler(request, h);
-      }
-      if (action === searchAgreementMessages.action) {
-        return searchAgreementMessages.handler(request, h);
-      }
-      if (action === searchClaimMessages.action) {
-        return searchClaimMessages.handler(request, h);
-      }
-      if (action === searchAgreementLogs.action) {
-        return searchAgreementLogs.handler(request, h);
-      }
+      return actionHandlers[action](request, h);
     },
   },
 };
