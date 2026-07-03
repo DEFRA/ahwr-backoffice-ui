3d65807cd0154e2be48f332508d306ccc2e910f7
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2026-07-03 10:39:07 +0100

    fix: AHWR-1967 removed debugging federated credentials logging #patch

--- app/auth/azure-auth.js
+++ app/auth/azure-auth.js
@@ -6,13 +6,9 @@ import { WebIdentityTokenProvider } from "@defra/hapi-auth-oidc";
 const wrapLoggerForPino = (logger) => ({
   info: (...args) => logger.info(...args),
   warn: (...args) => logger.warn(...args),
-  error: (firstArg, secondArg) => {
-    if (typeof firstArg === "string" && secondArg instanceof Error) {
-      logger.error({ error: secondArg }, firstArg);
-    } else {
-      logger.error(firstArg, secondArg);
-    }
-  },
+  // WebIdentityTokenProvider calls error(message, Error) but pino expects error({ err }, message)
+  error: (msg, err) =>
+    err instanceof Error ? logger.error({ error: err }, msg) : logger.error(msg, err),
 });
 
 export const getMsalLoggingSetup = () => {
@@ -47,17 +43,15 @@ export const init = () => {
     const logger = getLogger();
 
     logger.info("Initialising auth provider");
+    // Bug in library where audience should be an array but expected type is string
     const authProvider = new WebIdentityTokenProvider({ audience: ["ahwr-backoffice-ui"] });
-    logger.info("Initialised auth provider");
 
     auth = {
       clientId: config.auth.clientId,
       authority: config.auth.authority,
       clientAssertion: async () => {
-        logger.info("Retrieving credentials");
-        const assertion = await authProvider.getCredentials(wrapLoggerForPino(logger));
-        logger.info(`Retrieved credentials: ${assertion.slice(0, 4)}`);
-        return assertion;
+        logger.info("Retrieving federated credentials");
+        return authProvider.getCredentials(wrapLoggerForPino(logger));
       },
     };
   } else {
@@ -95,7 +89,7 @@ export const authenticate = async (redirectCode, auth, cookieAuth) => {
 export const logout = async (account) => {
   try {
     await msalApplication.getTokenCache().removeAccount(account);
-  } catch (err) {
-    console.error("Unable to end session", err);
+  } catch (error) {
+    getLogger().error({ error }, "Unable to end session");
   }
 };
