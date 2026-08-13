6a9f426d494cfafa6601b9b2439d1c9db5305f61
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-08-13 10:11:01 +0100

    Adding plugin and rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import neostandard from "neostandard";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import importPlugin from "eslint-plugin-import";
+import noCommentedCode from "eslint-plugin-no-commented-code";
 
 export default [
   ...neostandard({
@@ -12,6 +13,12 @@ export default [
     plugins: { import: importPlugin },
     rules: {
       "import/extensions": ["error", "always", { ignorePackages: true }],
+    },
+  },
+  {
+    plugins: { "no-commented-code": noCommentedCode },
+    rules: {
+      "no-commented-code/no-commented-code": "error",
     },
   },
 ];
