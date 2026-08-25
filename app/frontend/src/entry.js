3eefb0c486efef16328fec786960792e7e710367
Duncan Sommerville <233909619+sommerville-ee-defra@users.noreply.github.com>
2026-08-25 09:36:52 +0100

    refactor: (AHWR-2118) remove vestigial jquery dependency #patch

--- app/frontend/src/entry.js
+++ app/frontend/src/entry.js
@@ -2,10 +2,8 @@ import { initAll } from "govuk-frontend";
 import "./css/application.scss";
 import "./js/cookies.js";
 import "./js/handleDuplicateFormSubmissions.js";
-import $ from "jquery";
 import * as moj from "@ministryofjustice/frontend";
 initAll();
-window.$ = $;
 moj.initAll();
 function submitSort(dataUrl, sort) {
   const xhr = new XMLHttpRequest();
