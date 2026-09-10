b46d9d54377590e37a891d2c57e36a5036b566a2
Ross J <ross.jarvis@esynergy.co.uk>
2026-09-10 14:41:52 +0100

    feat: AHWR-2206 removed status filter from agreements search (#250)

--- app/routes/utils/get-agreement-type-options.js
+++ app/routes/utils/get-agreement-type-options.js
@@ -1,15 +1,9 @@
-import { AGREEMENT_TYPE, AGREEMENT_STATUS, FLAG } from "../../constants/index.js";
+import { AGREEMENT_TYPE, FLAG } from "../../constants/index.js";
 
 const agreementTypeLabels = {
   [AGREEMENT_TYPE.ALL]: "All types",
   [AGREEMENT_TYPE.IAHW]: "Improve Animal Health and Welfare (IAHW)",
   [AGREEMENT_TYPE.PBR]: "Poultry Biosecurity Review (PBR)",
-};
-
-const statusLabels = {
-  [AGREEMENT_STATUS.ALL]: "All statuses",
-  [AGREEMENT_STATUS.AGREED]: "Agreed",
-  [AGREEMENT_STATUS.NOT_AGREED]: "Not agreed",
 };
 
 const flagLabels = {
@@ -23,13 +17,6 @@ export const getAgreementTypeOptions = (selectedAgreementType) =>
     value,
     text,
     selected: value === selectedAgreementType,
-  }));
-
-export const getStatusOptions = (selectedStatus) =>
-  Object.entries(statusLabels).map(([value, text]) => ({
-    value,
-    text,
-    selected: value === selectedStatus,
   }));
 
 export const getFlagOptions = (selectedFlag) =>
